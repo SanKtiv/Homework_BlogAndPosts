2a51a3d6cae269b85186f05cb36e58aace6ad55f
SanKtiv <aktitorov@gmail.com>
2023-10-18 19:32:38 +0300

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express'
+import {videosRouter} from './routers/routers';
+
+const app = express()
+const port = process.env.PORT || 5000
+
+const  parserMiddleware = express.json()
+app.use(parserMiddleware)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('HI SAMURAI')
+})
+
+app.use('/', videosRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
