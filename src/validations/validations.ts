8c6a8d5b083a2a2b8d6b71805140f1f6d8121365
SanKtiv <aktitorov@gmail.com>
2023-10-22 13:49:51 +0300

    fix errorMessage, checking spaces

--- src/validations/validations.ts
+++ src/validations/validations.ts
@@ -3,19 +3,23 @@ import {regexp} from "../variables/variables"
 import {NextFunction, Request, Response} from 'express'
 
 export const validId = param('id', 'id is incorrect')
+    .trim()
     .isString()
 
 export const validName = body('name', 'name length is incorrect')
-    .isLength({min: 1, max: 15})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 15})
 
 export const validDescription = body('description', 'description length is incorrect')
-    .isLength({min: 1, max: 500})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 500})
 
 export const validWebsiteUrl = body('websiteUrl', 'URL adress is incorrect')
-    .isLength({min: 1, max: 100})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 100})
     .matches(regexp)
 
 export const validAuthorize = (req: Request, res: Response, next: NextFunction) => {
@@ -25,16 +29,20 @@ export const validAuthorize = (req: Request, res: Response, next: NextFunction) 
 }
 ///////////////////////////////////////
 export const validTitle = body('title', 'title length is incorrect')
-    .isLength({min: 1, max: 30})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 30})
 
 export const validShortDescription = body('shortDescription', 'shortDescription length is incorrect')
-    .isLength({min: 1, max: 100})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 100})
 
 export const validContent = body('content', 'content length is incorrect')
-    .isLength({min: 1, max: 1000})
+    .trim()
     .isString()
+    .isLength({min: 1, max: 1000})
 
 export const validBlogId = body('blogId', 'blogId length is incorrect')
+    .trim()
     .isString()
