735c4f6de274a1385af4947fa53e2263c529e7c3
SanKtiv <aktitorov@gmail.com>
2023-10-26 21:29:24 +0300

    fix db.ts

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv'
 import {MongoClient} from 'mongodb'
 dotenv.config()
 
-const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017'
+const mongoURI = process.env.MONGO_URL
 
 export const client = new MongoClient(mongoURI)
 
 export async function runDb() {
     try {
         await client.connect()
-        await client.db('tube').command({ping: 1})
+        //await client.db('tube').command({ping: 1})
         console.log('Connect successfully to mongo server')
     } catch {
         await client.close()
