b83ce5c1311b8d6eec0a357a31f08f19d843edd3
SanKtiv <aktitorov@gmail.com>
2023-11-11 23:48:12 +0300

    add services

---
+++ src/services/users-service.ts
@@ -0,0 +1,7 @@
+import {IdUserType, InputUserType, UserType} from "../types/types-users";
+
+export const userService = {
+    async createUser(body: InputUserType): Promise<IdUserType> {
+
+    }
+}
