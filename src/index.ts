b94b481416ece5b0b0135b6d81dbb13a404d1fe1
SanKtiv <aktitorov@gmail.com>
2023-11-28 23:54:05 +0300

    add ngrok

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import {app} from './setting';
 import {runDb} from "./repositories/mongodb-repository/db";
-import ngrok from 'ngrok'
 
+const ngrok = require('ngrok')
 const port = process.env.PORT || 3000
 
-
 const startApp = async () => {
-    // const url = await ngrok.connect(3000)
-    // console.log(url)
+    await ngrok.connect(3000)
     await runDb()
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
