ad95437aa3f99c0b201a9dd9bc51f3f150ffc08f
SanKtiv <aktitorov@gmail.com>
2024-03-15 10:58:37 +0300

    add class PostHandlers

--- src/routers/posts/post-handler.ts
+++ src/routers/posts/post-handler.ts
@@ -5,7 +5,7 @@ import {
     ViewPostsPagingType
 } from "../../types/posts-types";
 
-export const postHandlers = {
+class PostHandlers {
 
     createPostViewModelNew(postFromDB: PostDBType, userId?: string): ViewPostModelType {
 
@@ -35,7 +35,7 @@ export const postHandlers = {
                 newestLikes: newestLikes
             }
         }
-    },
+    }
 
     createPostPagingViewModelNew(totalPosts: number,
                                  postsFromDB: PostDBType[],
@@ -53,5 +53,57 @@ export const postHandlers = {
             totalCount: totalPosts,
             items: items
         }
-    },
+    }
 }
+
+export const postHandlers = new PostHandlers()
+// export const postHandlers = {
+//
+//     createPostViewModelNew(postFromDB: PostDBType, userId?: string): ViewPostModelType {
+//
+//         const {_id, userLikesInfo, extendedLikesInfo, ...postViewModelWithoutId} = postFromDB
+//
+//         let myStatus = 'None'
+//
+//         if (userId) {
+//
+//             const findUserStatus = userLikesInfo.find(el => el.userId === userId)
+//
+//             myStatus = findUserStatus ? findUserStatus.userStatus : 'None'
+//         }
+//
+//         const newestLikes = userLikesInfo
+//             .filter(el => el.userStatus === 'Like')
+//             .sort((a: any, b: any) => new Date(b.addedAt).getTime() - new Date(a.addedAt).getTime())
+//             .slice(0, 3)
+//             .map(({userStatus, ...newestLikes}) => newestLikes)
+//
+//         return  {
+//             id: postFromDB._id.toString(),
+//             ...postViewModelWithoutId,
+//             extendedLikesInfo: {
+//                 ...extendedLikesInfo,
+//                 myStatus: myStatus,
+//                 newestLikes: newestLikes
+//             }
+//         }
+//     },
+//
+//     createPostPagingViewModelNew(totalPosts: number,
+//                                  postsFromDB: PostDBType[],
+//                                  query: InputPostsPagingType,
+//                                  userId?: string): ViewPostsPagingType {
+//
+//         const items = userId ?
+//             postsFromDB.map(postDB => this.createPostViewModelNew(postDB, userId)) :
+//             postsFromDB.map(postDB => this.createPostViewModelNew(postDB))
+//
+//         return {
+//             pagesCount: Math.ceil(totalPosts / +query.pageSize),
+//             page: +query.pageNumber,
+//             pageSize: +query.pageSize,
+//             totalCount: totalPosts,
+//             items: items
+//         }
+//     },
+// }
