4c3f77162110990a897025e894c03c20906ed89f
SanKtiv <aktitorov@gmail.com>
2024-03-21 09:47:23 +0300

    add class PostsRepositoryQuery

--- src/repositories/mongodb-repository/posts-mongodb/posts-query-mongodb.ts
+++ src/repositories/mongodb-repository/posts-mongodb/posts-query-mongodb.ts
@@ -2,13 +2,12 @@ import {dbPostsCollection} from "../db";
 import {ObjectId} from "mongodb";
 import {PostDBType} from "../../../types/posts-types";
 
-
-export const postsRepositoryQuery = {
+class PostsRepositoryQuery {
 
     async getPostsTotalCount(): Promise<number> {
 
         return dbPostsCollection.countDocuments()
-    },
+    }
 
     async getPostsWithPaging(query: any): Promise<PostDBType[]> {
 
@@ -18,24 +17,24 @@ export const postsRepositoryQuery = {
             .skip((+query.pageNumber - 1) * +query.pageSize)
             .limit(+query.pageSize)
             .toArray()
-    },
+    }
 
     async getPostById(id: string): Promise<PostDBType | null> {
 
         try {return dbPostsCollection.findOne({_id: new ObjectId(id)})}
 
         catch (error) {return null}
-    },
+    }
 
-    async getLikesInfoByPostId(id: string): Promise<any | null> {
+    async getLikesInfoByPostId(id: string): Promise<PostDBType | null> {
 
         try {
             return dbPostsCollection
                 .findOne({_id: new ObjectId(id)},
-            {projection: {_id: 0, extendedLikesInfo: 1}})
+                    {projection: {_id: 0, extendedLikesInfo: 1}})
         }
         catch (error) {return null}
-    },
+    }
 
     async getPostUserLikeStatusByPostId(postId: string, userId: string): Promise<PostDBType | null> {
 
@@ -52,5 +51,58 @@ export const postsRepositoryQuery = {
                 })
         }
         catch (error) {return null}
-    },
+    }
 }
+
+export const postsRepositoryQuery = new PostsRepositoryQuery()
+// export const postsRepositoryQuery = {
+//
+//     async getPostsTotalCount(): Promise<number> {
+//
+//         return dbPostsCollection.countDocuments()
+//     },
+//
+//     async getPostsWithPaging(query: any): Promise<PostDBType[]> {
+//
+//         return dbPostsCollection
+//             .find()
+//             .sort({createdAt: query.sortDirection})
+//             .skip((+query.pageNumber - 1) * +query.pageSize)
+//             .limit(+query.pageSize)
+//             .toArray()
+//     },
+//
+//     async getPostById(id: string): Promise<PostDBType | null> {
+//
+//         try {return dbPostsCollection.findOne({_id: new ObjectId(id)})}
+//
+//         catch (error) {return null}
+//     },
+//
+//     async getLikesInfoByPostId(id: string): Promise<PostDBType | null> {
+//
+//         try {
+//             return dbPostsCollection
+//                 .findOne({_id: new ObjectId(id)},
+//             {projection: {_id: 0, extendedLikesInfo: 1}})
+//         }
+//         catch (error) {return null}
+//     },
+//
+//     async getPostUserLikeStatusByPostId(postId: string, userId: string): Promise<PostDBType | null> {
+//
+//         try {
+//             return dbPostsCollection.findOne({
+//                     _id: new ObjectId(postId),
+//                     'userLikesInfo.userId': userId
+//                 },
+//                 {
+//                     projection: {
+//                         _id: 0,
+//                         'userLikesInfo.$': 1
+//                     }
+//                 })
+//         }
+//         catch (error) {return null}
+//     },
+// }
