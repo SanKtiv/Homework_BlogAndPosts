5c4e2a1f6a97bb823cc588d5b7049fcf52e159b2
SanKtiv <aktitorov@gmail.com>
2024-03-21 09:31:05 +0300

    add class CommentsRepository

--- src/repositories/mongodb-repository/comments-mongodb/comments-command-mongodb.ts
+++ src/repositories/mongodb-repository/comments-mongodb/comments-command-mongodb.ts
@@ -2,21 +2,24 @@ import {CommentDBType, CommentType, LikesInfoType} from "../../../types/comments
 import {CommentModel} from "../db";
 import {ObjectId} from "mongodb";
 
-export const commentsRepository = {
+class CommentsRepository {
 
     async insertComment(comment: CommentType): Promise<CommentDBType> {
 
         const commentDB = await CommentModel.create(comment)
 
         return commentDB as CommentDBType
-    },
+    }
 
     async updateCommentContentById(id: string, content: string): Promise<void> {
 
         await CommentModel.findOneAndUpdate({_id: new ObjectId(id)}, {$set: {content: content}})
-    },
+    }
 
-    async updateCommentAddNewUserLikeStatus(commentId: string, userId: string, status: string, likesInfo: LikesInfoType): Promise<void> {
+    async updateCommentAddNewUserLikeStatus(commentId: string,
+                                            userId: string,
+                                            status: string,
+                                            likesInfo: LikesInfoType): Promise<void> {
 
         await CommentModel.updateOne({_id: new ObjectId(commentId)},
             {
@@ -26,9 +29,12 @@ export const commentsRepository = {
                 },
                 $push: {usersLikeStatuses: {userId: userId, userStatus: status}}
             })
-    },
+    }
 
-    async updateCommentLikesInfoByCommentId(commentId: string, userId: string, status: string, likesInfo: LikesInfoType): Promise<void> {
+    async updateCommentLikesInfoByCommentId(commentId: string,
+                                            userId: string,
+                                            status: string,
+                                            likesInfo: LikesInfoType): Promise<void> {
 
         await CommentModel
             .findOneAndUpdate({_id: new ObjectId(commentId), 'usersLikeStatuses.userId': userId},
@@ -39,14 +45,65 @@ export const commentsRepository = {
                         'usersLikeStatuses.$.userStatus': status
                     }
                 })
-    },
+    }
 
     async deleteCommentById(id: string): Promise<void> {
         await CommentModel.deleteOne({_id: new ObjectId(id)})
-    },
+    }
 
     async deleteAll(): Promise<void> {
 
         await CommentModel.deleteMany({})
     }
 }
+
+export const commentsRepository = new CommentsRepository()
+
+// export const commentsRepository = {
+//
+//     async insertComment(comment: CommentType): Promise<CommentDBType> {
+//
+//         const commentDB = await CommentModel.create(comment)
+//
+//         return commentDB as CommentDBType
+//     },
+//
+//     async updateCommentContentById(id: string, content: string): Promise<void> {
+//
+//         await CommentModel.findOneAndUpdate({_id: new ObjectId(id)}, {$set: {content: content}})
+//     },
+//
+//     async updateCommentAddNewUserLikeStatus(commentId: string, userId: string, status: string, likesInfo: LikesInfoType): Promise<void> {
+//
+//         await CommentModel.updateOne({_id: new ObjectId(commentId)},
+//             {
+//                 $set: {
+//                     'likesInfo.likesCount': likesInfo.likesCount,
+//                     'likesInfo.dislikesCount': likesInfo.dislikesCount,
+//                 },
+//                 $push: {usersLikeStatuses: {userId: userId, userStatus: status}}
+//             })
+//     },
+//
+//     async updateCommentLikesInfoByCommentId(commentId: string, userId: string, status: string, likesInfo: LikesInfoType): Promise<void> {
+//
+//         await CommentModel
+//             .findOneAndUpdate({_id: new ObjectId(commentId), 'usersLikeStatuses.userId': userId},
+//                 {
+//                     $set: {
+//                         'likesInfo.likesCount': likesInfo.likesCount,
+//                         'likesInfo.dislikesCount': likesInfo.dislikesCount,
+//                         'usersLikeStatuses.$.userStatus': status
+//                     }
+//                 })
+//     },
+//
+//     async deleteCommentById(id: string): Promise<void> {
+//         await CommentModel.deleteOne({_id: new ObjectId(id)})
+//     },
+//
+//     async deleteAll(): Promise<void> {
+//
+//         await CommentModel.deleteMany({})
+//     }
+// }
